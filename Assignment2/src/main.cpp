2c5e312195875d14335fdc45cbe752794c63dbe0
th1nk2r-git <th1nk2r@qq.com>
2026-01-22 03:14:16 +0800

    完成了作业2，修复了作业1中投影变换y轴颠倒的错误

--- Assignment2/src/main.cpp
+++ Assignment2/src/main.cpp
@@ -1,8 +1,9 @@
-#include "Triangle.hpp"
-#include "rasterizer.hpp"
-#include <eigen3/Eigen/Eigen>
+// clang-format off
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "rasterizer.hpp"
+#include "global.hpp"
+#include "Triangle.hpp"
 
 constexpr double MY_PI = 3.1415926;
 
@@ -10,7 +11,9 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos) {
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1, -eye_pos[2],
+    translate << 1, 0, 0, -eye_pos[0],
+        0, 1, 0, -eye_pos[1],
+        0, 0, 1, -eye_pos[2],
         0, 0, 0, 1;
 
     view = translate * view;
@@ -20,11 +23,6 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos) {
 
 Eigen::Matrix4f get_model_matrix(float rotation_angle) {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
-
-    model(0, 0) = cos(rotation_angle);
-    model(0, 1) = -sin(rotation_angle);
-    model(1, 0) = sin(rotation_angle);
-    model(1, 1) = cos(rotation_angle);
     return model;
 }
 
@@ -58,32 +56,45 @@ int main(int argc, const char** argv) {
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
+    if (argc == 2) {
         command_line = true;
-        angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
-            filename = std::string(argv[3]);
-        }
-        else
-            return 0;
+        filename = std::string(argv[1]);
     }
 
     rst::rasterizer r(700, 700);
 
-    Eigen::Vector3f eye_pos = { 0, 0, 5 };
+    Eigen::Vector3f eye_pos = { 0,0,5 };
 
-    std::vector<Eigen::Vector3f> pos {
-        {2, 0, -2},
-        {0, 2, -2},
-        {-2, 0, -2}
+
+    std::vector<Eigen::Vector3f> pos
+    {
+            {2, 0, -2},
+            {0, 2, -2},
+            {-2, 0, -2},
+            {3.5, -1, -5},
+            {2.5, 1.5, -5},
+            {-1, 0.5, -5}
     };
 
-    std::vector<Eigen::Vector3i> ind {
-        {0, 1, 2}
+    std::vector<Eigen::Vector3i> ind
+    {
+            {0, 1, 2},
+            {3, 4, 5}
+    };
+
+    std::vector<Eigen::Vector3f> cols
+    {
+            {217.0, 238.0, 185.0},
+            {217.0, 238.0, 185.0},
+            {217.0, 238.0, 185.0},
+            {185.0, 217.0, 238.0},
+            {185.0, 217.0, 238.0},
+            {185.0, 217.0, 238.0}
     };
 
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
+    auto col_id = r.load_colors(cols);
 
     int key = 0;
     int frame_count = 0;
@@ -95,9 +106,10 @@ int main(int argc, const char** argv) {
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
 
         cv::imwrite(filename, image);
 
@@ -111,22 +123,17 @@ int main(int argc, const char** argv) {
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
 
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
         cv::imshow("image", image);
         key = cv::waitKey(10);
 
         std::cout << "frame count: " << frame_count++ << '\n';
-
-        if (key == 'a') {
-            angle += 10;
-        }
-        else if (key == 'd') {
-            angle -= 10;
-        }
     }
 
     return 0;
 }
+// clang-format on
